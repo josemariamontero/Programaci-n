b69785c1553ee0a439bf3207a6b6c900886b92b2
José María Montero Romero <josemaria@daryl>
2020-10-16 23:55:12 +0200

    Terminado

--- primerosejercicios/primerosejercicios/ejercicio4.java
+++
@@ -1,7 +0,0 @@
-public class ejercicio4 {
-	public static void main(String[] args) {
-		System.out.printf("El numero %d no tiene decimales. \n", 10);
-		System.out.printf("El numero %f tiene decimales. \n", 21.3);
-		System.out.printf("El %.2f tiene 2 decimales. \n", 22.4);
-	}
-}
